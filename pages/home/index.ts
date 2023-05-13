98a54d394041d5c20957155cdd0f481a64861161
moayad-khader <moayadkhader99@gmail.com>
2023-05-14 00:57:20 +0300

    add: home page

--- pages/home/index.ts
+++ pages/home/index.ts
@@ -7,13 +7,14 @@ const mapStateToProps = (state: any) => {
     return {
         repositories: state.RepositoryState.repositories
     };
-  };
-  const mapDispatchToProps = (dispatch: any) => {
+};
+
+const mapDispatchToProps = (dispatch: any) => {
     return {
-      getAllModules: (query:string) =>
+      getAllRepositories: (query:string) =>
         dispatch({ type: RepositoryActionTypes.GET_ALL_REPOSITORIES, query})
     };
-  };
+};
   
 
 export default connect(mapStateToProps,mapDispatchToProps)(Page);
--- pages/home/page.tsx
+++ pages/home/page.tsx
@@ -1,9 +1,16 @@
 import type { NextPage } from 'next'
 import Header from 'common/components/header';
+import { IRepository } from 'store/Repository/repository.types';
 
+interface Props {
+    repositories: Array<IRepository>,
+    getAllRepositories(query:string): void
+}
 
-
-const Home: NextPage = () => {
+const Home: NextPage<Props> = ({
+    repositories,
+    getAllRepositories
+}) => {
   return (
     <div className="min-h-screen  w-screen flex flex-col justify-between items-center relative bg-gray-800">
       <Header
