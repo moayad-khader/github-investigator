72bfc381699185ef9ceae9ace5aae56f64e7849f
moayad-khader <moayadkhader99@gmail.com>
2023-05-14 00:46:00 +0300

    add: repos store

--- store/reducer.ts
+++ store/reducer.ts
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
-
+import repositoryReducer from "./Repository/repository.reducers";
 
 const rootReducer = combineReducers({
-
+    RepositoryState: repositoryReducer
 });
 
 export default rootReducer;
